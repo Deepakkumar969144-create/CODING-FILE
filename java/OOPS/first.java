b72cc334793053318851c5c4e5060874fd2cedd7
Deepakkumar969144-create <dkp969144@gmail.com>
2026-04-13 17:48:34 +0530

    first commit

--- java/OOPS/first.java
+++
@@ -1,7 +0,0 @@
-package java.OOPS;
-import java.util.Scanner;
-import java.util.*;
-
-public class first {
-    
-}
---
+++ taiyarijavaa/taioop/userdef.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+import java.util.*;
+import java.util.*;
+
+public class userdef {
+    public static void main(String[] args) {
+        System.out.println("Hello DK ");
+    }
+}
